a521856b379677fa7b06095c49e002d5fc25c6b0
NguyenVanMinh <vanminhok12@gmail.com>
2022-11-06 15:05:41 +0700

    Minh

---
+++ Emerge.js
@@ -0,0 +1,14 @@
+var fullName='Nguyen Van Minh';
+alert(fullName);
+confirm('Press the button to confirm the information');
+prompt("Type your phone number")
+console.log('fullName');
+console.error(fullName);
+console.warn(fullname);
+setTimeout(function() { alert(fullName)
+}, 1200);
+setInterval(function() { alert(fullName)
+}, 1200);
+/**
+ *
+ */
